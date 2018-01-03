2272b1daf3ad67ec513942099f083368a34a6db3
kristof66635 <christopheplanchais@gmail.com>
2018-01-03 23:42:12 +0100

    fourth

--- src/main/java/fr/istic/Display.java
+++ src/main/java/fr/istic/Display.java
@@ -1,17 +1,25 @@
 package fr.istic;
 
 import fr.istic.activeObject.Canal;
+import fr.istic.activeObject.Generator;
 import fr.istic.activeObject.GeneratorAsync;
 import fr.istic.observer.ObserverGeneratorAsync;
 import javafx.scene.control.Label;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Display implements ObserverGeneratorAsync {
 
     Canal canal;
     Future<Integer> future;
     Label lebel;
+
+    public Display(Canal canal) {
+        this.canal = canal;
+    }
 
     public Display(Canal canal, Label lebel) {
         this.canal = canal;
@@ -37,5 +45,18 @@ public class Display implements ObserverGeneratorAsync {
         return null;
     }
 
+    public static void main(String[] args){
+        ScheduledExecutorService scheduledExecutorService;
+        scheduledExecutorService = new ScheduledThreadPoolExecutor(20);
+        Canal canal;
+         Display display;
+         Generator generator;
+        generator=new Generator();
+        canal=new Canal(scheduledExecutorService,generator);
+        display=new Display(canal);
+        System.out.println("Display.getValueFuture():Future");
+        scheduledExecutorService.scheduleAtFixedRate(generator::createvalue,0,100, TimeUnit.MILLISECONDS);
 
+
+    }
 }
